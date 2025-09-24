3e264174cc4c9c9139af7d9a32133f635d7c2dd5
CristianNorga <cristian.rraga95@hotmail.com>
2025-09-24 06:50:14 -0500

    fix: filling board, remove condition length, add deteccion slot empty

--- linear_board.py
+++ linear_board.py
@@ -9,20 +9,21 @@ class LinearBoard():
       return board
 
   def __init__(self) -> None:
-    self._column = []
+    self._column = [None for i in range(BOARD_LENGTH)]
 
   def as_list(self) -> list:
     return self._column
 
   def is_full(self) -> bool:
-    return len(self._column) >= BOARD_LENGTH and all(map(lambda x: x != None, self._column))
+    return all(map(lambda x: x != None, self._column))
 
   def is_victory(self, user_notation) -> bool:
     return find_streak(self._column, user_notation, VICTORY_STRIKE)
   
   def add(self, user_notation) -> None:
     if not self.is_full():
-      self._column.append(user_notation)
+      i = self._column.index(None)
+      self._column[i] = user_notation
 
   def is_tie(self, user1_notation, user2_notation) -> bool:
     return self.is_victory(user1_notation) == False and self.is_victory(user2_notation) == False
