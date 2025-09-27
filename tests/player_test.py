9dad2c09064bbfd175aab99408ba527b2657868c
CristianNorga <cristian.rraga95@hotmail.com>
2025-09-27 16:20:19 -0500

    fix: unit test player, increment length board

--- tests/player_test.py
+++ tests/player_test.py
@@ -4,7 +4,7 @@ from game.models.oracle import BaseOracle
 from game.models.player import Player
 
 def test_play():
-    before = SquareBoard.fromList(
+    board = SquareBoard.fromList(
         [
             [None, None, None, None],
             ['x', 'o', 'x', 'o'],
@@ -13,16 +13,9 @@ def test_play():
         ]
     )
 
-    after = SquareBoard.fromList(
-        [
-            ['x', None, None, None],
-            ['x', 'o', 'x', 'o'],
-            ['x', 'o', 'x', 'o'],
-            ['x', None, None, None]
-        ]
-    )
+    number_of_empty = sum(1 for row in board.columns_as_lists() for cell in row if cell is None)
 
     player = Player('Chip', 'x', oracle = BaseOracle())
 
-    player.play(before)
-    assert before == after
+    player.play(board)
+    assert number_of_empty - 1 == sum(1 for row in board.columns_as_lists() for cell in row if cell is None)
